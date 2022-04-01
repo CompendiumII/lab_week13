95dfb31f1d78228d88b6bb335a704ad590870068
Kevin X <tnkgavas363@gmail.com>
2022-04-01 16:34:02 -0700

    Added styling to index.html. NO ACCESSIBILITY ISSUES I THINK !!!!!!!

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-const originDiv = document.querySelectorAll(".original-container div")          // Selects the divs within class original-container
-const cloneDiv = document.querySelector(".copy-container")                      // Selects the class copy-container
-const clrBtn = document.querySelector("button")                                 // Selects the only button
 
-originDiv.forEach(iterate => iterate.addEventListener('click', (evt) => {
-    const itemBeingCopied = evt.target.parentElement.outerHTML                  // We set a new object to be equivalent to the item we are copying
-    cloneDiv.insertAdjacentHTML("beforeend", itemBeingCopied)                   // And then we add it into the copy-container
+// CLONE
+const originDiv = document.querySelectorAll(".original-container div")            // Selects the divs within class original-container
+const cloneDiv = document.querySelector(".copy-container")                        // Selects the class copy-container
+
+originDiv.forEach(lookThrough => lookThrough.addEventListener('click', (evt) => { // When we register a click on any of the items within the original-container, we perform the following...
+    const itemBeingCopied = evt.target.parentElement.outerHTML                    // We set a new object to be equivalent to the item we are copying
+    cloneDiv.insertAdjacentHTML("beforeend", itemBeingCopied)                     // And then we add it into the copy-container
 }))
 
-clearArea = () => cloneDiv.innerHTML = "";                                      // Creates a function to clear the innerHTML of the cloneDiv div.
-clrBtn.addEventListener('click', clearArea)                                     // When click on the clear button is registered, we execute the function clearArea.
+// DELETE
+const clrBtn = document.querySelector("button")                                   // Selects the only button
+
+clearArea = () => cloneDiv.innerHTML = "";                                        // Creates a function to clear the innerHTML of the cloneDiv div.
+clrBtn.addEventListener('click', clearArea)                                       // When click on the clear button is registered, we execute the function clearArea.
